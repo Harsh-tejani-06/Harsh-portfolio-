bdd42c7436d7b42e9505cd839de15716c94736ff
Harsh-tejani-06 <harshtejani06@gmail.com>
2026-03-03 15:03:24 +0530

    fix: install lucide-react dependency

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['lucide-react']
+  },
+  build: {
+    rollupOptions: {
+      external: ['lucide-react']
+    }
+  }
 })
